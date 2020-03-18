a60cdbdeab22885e285264d2da32bcbccafbc4e5
Craig <craigoda@gmail.com>
2020-03-18 12:13:30 -0700

    deleted list files

--- bin/main.dart
+++ bin/main.dart
@@ -2,13 +2,18 @@ import 'package:apitest/get_info.dart';
 import 'package:args/args.dart';
 import 'package:apitest/post_state.dart';
 
+
 void main(List<String> args) async {
   print(args);
   var parser = ArgParser();
   parser.parse(args);
 
-  // RICOH THETA info
-  String url = 'http://192.168.1.1/osc/info';
-  // getInfo(url);
+  /// RICOH THETA info 
+  /// Official API reference https://api.ricoh/docs/theta-web-api-v2.1/protocols/info/
+  /// uncomment the line below to test info API
+  // getInfo();
+
+  /// camera state
+  /// API reference https://api.ricoh/docs/theta-web-api-v2.1/protocols/state/
   postState();
 }
