7246effb9de921374840f1fef57cd89bc40820f2
craig <craig@oppkey.com>
2021-01-28 19:41:49 -0800

    changed info to use http package eliminated pretty print custom library in favor of  JsonEncoder.withIndent()

--- lib/protocols/info.dart
+++ lib/protocols/info.dart
@@ -1,23 +1,22 @@
-import 'dart:io';
 import 'dart:convert';
-import 'package:apitest/pretty_print.dart';
+import 'package:http/http.dart' as http;
 
 void getInfo() async {
-
-  
   // test data to return a single JSON objects from Internet
   // String url = 'https://swapi.co/api/people/1';
   // String url = 'https://jsonplaceholder.typicode.com/users/1';
 
   String url = 'http://192.168.1.1/osc/info';
 
-  var request = await HttpClient().getUrl(Uri.parse(url));
-  // sends the request
-  var response = await request.close(); 
-
-  // transforms and prints the response
-  await for (var contents in response.transform(Utf8Decoder())) {
-    prettyPrint(contents.toString());
-  }
-
+  http.Response response = await http.get(url);
+  // the response.body is a String which can be printed as is.
+  // the next lines format the String into more
+  // human-readable form
+  // there's a nice gist here
+  // https://gist.github.com/kasperpeulen/d61029fc0bc6cd104602
+  Map responseBody = jsonDecode(response.body);
+  // print the map with nice formatting
+  print(JsonEncoder.withIndent('  ').convert(responseBody));
+  // print single value from map
+  print("firmware: ${responseBody['firmwareVersion']}");
 }
