abe79e81968ca9cbbacd04a52f9f98b68082f0c4
Craig <craig@oppkey.com>
2021-01-31 07:41:01 -0800

    new article on HDR settings

--- lib/options/filter_off.dart
+++ lib/options/filter_off.dart
@@ -1,16 +1,15 @@
-/// disable exposure delay
-/// 
+/// disable filter
+///
 /// https://api.ricoh/docs/theta-web-api-v2.1/commands/camera.set_options/
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:apitest/pretty_print.dart';
 
+Future<http.Response> filterOff() async {
+  var url = 'http://192.168.1.1/osc/commands/execute';
 
-Future<http.Response> filterOff () async {
-  var url ='http://192.168.1.1/osc/commands/execute';
-
- Map data = {
+  Map data = {
     'name': 'camera.setOptions',
     'parameters': {
       'options': {
@@ -23,9 +22,7 @@ Future<http.Response> filterOff () async {
   var body = jsonEncode(data);
 
   var response = await http.post(url,
-      headers: {"Content-Type": "application/json;charset=utf-8"},
-      body: body
-  );
+      headers: {"Content-Type": "application/json;charset=utf-8"}, body: body);
   print("${response.statusCode}");
   prettyPrint("${response.body}");
   return response;
--- lib/options/set_hdr.dart
+++ lib/options/set_hdr.dart
@@ -1,17 +1,14 @@
-/// disable exposure delay
-/// 
+///
 /// https://api.ricoh/docs/theta-web-api-v2.1/commands/camera.set_options/
-/// https://api.ricoh/docs/theta-web-api-v2.1/options/exposure_delay/
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:apitest/pretty_print.dart';
 
+Future<http.Response> setHdr() async {
+  var url = 'http://192.168.1.1/osc/commands/execute';
 
-Future<http.Response> setHdr () async {
-  var url ='http://192.168.1.1/osc/commands/execute';
-
- Map data = {
+  Map data = {
     'name': 'camera.setOptions',
     'parameters': {
       'options': {
@@ -24,9 +21,7 @@ Future<http.Response> setHdr () async {
   var body = jsonEncode(data);
 
   var response = await http.post(url,
-      headers: {"Content-Type": "application/json;charset=utf-8"},
-      body: body
-  );
+      headers: {"Content-Type": "application/json;charset=utf-8"}, body: body);
   print("${response.statusCode}");
   prettyPrint("${response.body}");
   return response;
